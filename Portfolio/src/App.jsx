a999b825aba62a0451a1378fcbc1f50142814781
Shaikh Mubashir <shaikhmubashirahmed123@gmail.com>
2026-05-19 02:31:28 +0530

    Customized Navbar

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
-import Navbar from "./Components/Pages/Navbar/Navbar";
+import Navbar from "./Components/Pages/Navbar/NavbarPages";
 
 function App() {
   return (
