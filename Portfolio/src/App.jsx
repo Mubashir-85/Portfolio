dc0e40a7a83d147f33780383b496be1b3463a6e9
Shaikh Mubashir <shaikhmubashirahmed123@gmail.com>
2026-05-20 23:29:31 +0530

    Refactor App component to use Routes for better structure

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,16 +1,11 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
+import Routes from "./Components/Routes/Routes";
 import "./App.css";
-import NavbarPages from "./Components/Pages/Navbar/NavbarPages";
-import HeroPage from "./Components/Pages/Hero/HeroPage";
 
 function App() {
   return (
     <>
-      <NavbarPages />
-      <HeroPage/>
+      <Routes/>
     </>
   );
 }
---
+++ Portfolio/src/Components/Routes/Routes.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavbarPages from '../Pages/Navbar/NavbarPages'
+import HeroPage from '../Pages/Hero/HeroPage'
+
+function Routes() {
+  return (
+    <>
+    <div>
+        <NavbarPages/>
+        <HeroPage/>
+    </div>
+    </>
+  )
+}
+
+export default Routes
