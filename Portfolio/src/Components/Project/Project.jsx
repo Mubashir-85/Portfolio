0207571b33a7c2674342617ec022f72d76b9f93e
Shaikh Mubashir <shaikhmubashirahmed123@gmail.com>
2026-06-02 20:11:00 +0530

    Enhance Project component layout and content for improved presentation of featured work

--- Portfolio/src/Components/Project/Project.jsx
+++ Portfolio/src/Components/Project/Project.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
 function Project() {
+    const projects = {
+        project1: {},
+        project2: {},
+        project3: {},
+    }
   return (
-    <div>Project</div>
-  )
+    <div className="h-screen bg-[#1E1D1B] flex ">
+      <div className="flex flex-col items-start justify-center w-full max-w-7xl mx-auto px-6 lg:px-20 gap-3">
+        <h1 className="font-bold text-white lg:text-5xl font-serif">
+          Featured Work
+        </h1>
+        <div className="text-[#D4C5AB] w-full h-1 mb-6">
+          <h2 className="text-[#D4C5AB]">
+            A selection of full-stack applications built focusing on scalable
+            architecture,
+          </h2>
+          <h2>seamless user experiences, and modern backend solutions.</h2>
+        </div>
+
+
+
+      </div>
+    </div>
+  );
 }
 
-export default Project
+export default Project;
