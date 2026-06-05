72b8daa2cd2e88c88a4c041e527a8da08ca1f621
Shaikh Mubashir <shaikhmubashirahmed123@gmail.com>
2026-06-05 20:56:43 +0530

    Add Footer component and integrate it into Layout for improved site structure

--- Portfolio/src/Components/Layout.jsx
+++ Portfolio/src/Components/Layout.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import NavbarPages from './Pages/Navbar/NavbarPages'
 import { Outlet } from 'react-router-dom'
 import HeroPage from './Pages/Hero/HeroPage'
+import FooterPage from './Pages/Footer/FooterPage'
 
 function Layout() {
   return (
     <>
     <NavbarPages/>
     <Outlet/>
+    <FooterPage/>
     
     </>
   )
---
+++ Portfolio/src/Components/Pages/Footer/FooterPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Footer from '../../Footer/Footer'
+
+function FooterPage() {
+  return (
+    <div><Footer/></div>
+  )
+}
+
+export default FooterPage
