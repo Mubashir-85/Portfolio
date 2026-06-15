c0fa0e013fefaa28e660c102066a429d6e98cf70
Shaikh Mubashir <shaikhmubashirahmed123@gmail.com>
2026-06-15 20:02:00 +0530

    Add TechStack component to HeroPage for technology showcase

--- Portfolio/src/Components/Pages/Hero/HeroPage.jsx
+++ Portfolio/src/Components/Pages/Hero/HeroPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Hero from "../../Hero/Hero";
+import TechStack from "../../Hero/TechStack";
 
 function HeroPage() {
   return <>
   <Hero/>
+  <TechStack/>
   </>
 }
 
